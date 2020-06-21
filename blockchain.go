e3b9a116f5502f8e6e17bbc24a3cc8a9869f3e25
tomoyaueno29 <u.t012807@gmail.com>
2020-06-21 17:02:17 +0900

    Update blockchain.go

--- blockchain.go
+++ blockchain.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 	"time"
 )
 
@@ -22,17 +23,16 @@ func NewBlockchain() *Blockchain {
 	bc := new(Blockchain)
 	bc.CreateBlock(0, "Init hash")
 	return bc
-	//bc.transactionPool = transactionPool
-	//bc.chain = chain
-	//return bc
 }
 
+//構造体Blockchainのスライス型のchainにNewBlockメソッドで初期化した構造体を追加している
 func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
 	b := NewBlock(nonce, previousHash)
 	bc.chain = append(bc.chain, b)
 	return b
 }
 
+//構造体Blockに値を代入して新しい構造体を初期化
 func NewBlock(nonce int, previousHash string) *Block {
 	b := new(Block)
 	b.timestamp = time.Now().UnixNano()
@@ -45,14 +45,26 @@ func (b *Block) Print() {
 	fmt.Printf("timestamp       %d\n", b.timestamp)
 	fmt.Printf("nonce       %d\n", b.nonce)
 	fmt.Printf("previousHash       %s\n", b.previousHash)
-	fmt.Printf("previousHash       %s\n", b.transactions)
+	fmt.Printf("transactions       %s\n", b.transactions)
 }
 
 func init() {
 	log.SetPrefix("Blockchain:")
 }
 
+func (bc *Blockchain) Print() {
+	for i, block := range bc.chain {
+		fmt.Println("Chain %d \n", strings.Repeat("=", 25), i, strings.Repeat("=", 25))
+		block.Print()
+	}
+	fmt.Printf("%s\n", strings.Repeat("*", 25))
+}
+
 func main() {
 	blockchain := NewBlockchain()
-	fmt.Println(blockchain)
+	blockchain.Print()
+	blockchain.CreateBlock(5, "hash 1")
+	blockchain.Print()
+	blockchain.CreateBlock(2, "hash 2")
+	blockchain.Print()
 }
