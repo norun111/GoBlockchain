8ab246c35813a6e6919a2bb58fae633f97ed0bca
tomoyaueno29 <u.t012807@gmail.com>
2020-06-21 16:45:05 +0900

    Update blockchain.go

--- blockchain.go
+++ blockchain.go
@@ -6,15 +6,6 @@ import (
 	"time"
 )
 
-func init() {
-	log.SetPrefix("Blockchain:")
-}
-
-func main() {
-	log.Println("test")
-	fmt.Println("test2")
-}
-
 type Block struct {
 	nonce int
 	previousHash string
@@ -28,4 +19,20 @@ func NewBlock(nonce int, previousHash string) *Block {
 	b.nonce = nonce
 	b.previousHash = previousHash
 	return b
+}
+
+func (b *Block) Print() {
+	fmt.Printf("timestamp       %d\n", b.timestamp)
+	fmt.Printf("nonce       %d\n", b.nonce)
+	fmt.Printf("previousHash       %s\n", b.previousHash)
+	fmt.Printf("previousHash       %s\n", b.transactions)
+}
+
+func init() {
+	log.SetPrefix("Blockchain:")
+}
+
+func main() {
+	b := NewBlock(0, "init hash")
+	fmt.Println(b)
 }
