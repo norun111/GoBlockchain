a42cb7e86002e24f9d06a89c42d99dbf6108f59a
tomoyaueno29 <u.t012807@gmail.com>
2020-06-21 16:41:17 +0900

    Update blockchain.go

--- blockchain.go
+++ blockchain.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 )
 
 func init() {
@@ -12,4 +13,19 @@ func init() {
 func main() {
 	log.Println("test")
 	fmt.Println("test2")
+}
+
+type Block struct {
+	nonce int
+	previousHash string
+	timestamp int64
+	transactions []string
+}
+
+func NewBlock(nonce int, previousHash string) *Block {
+	b := new(Block)
+	b.timestamp = time.Now().UnixNano()
+	b.nonce = nonce
+	b.previousHash = previousHash
+	return b
 }
