06b2dbba43f5fcd3072aabff8f4b85de7fc7ad84
tomoyaueno29 <u.t012807@gmail.com>
2020-06-22 18:36:54 +0900

    Update neighbor.go

--- utils/neighbor.go
+++ utils/neighbor.go
@@ -3,6 +3,8 @@ package utils
 import (
 	"fmt"
 	"net"
+	"regexp"
+	"strconv"
 	"time"
 )
 
@@ -15,4 +17,29 @@ func IsFoundHost(host string, port uint16) bool {
 		return false
 	}
 	return true
+}
+
+var PATTERN = regexp.MustCompile(`((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?\.){3})(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)`)
+
+func FindNeighbors(myHost string, myPort uint16, startIp uint8, endIp uint8, startPort uint16, endPort uint16) []string {
+	address := fmt.Sprintf("%s:%d", myHost, myPort)
+
+	m := PATTERN.FindStringSubmatch(myHost)
+	if m == nil {
+		return nil
+	}
+	prefixHost := m[1]
+	lastIp, _ := strconv.Atoi(m[len(m)-1])
+	neighbors := make([]string, 0)
+
+	for port := startPort; port <= endPort; port ++ {
+		for ip := startIp; ip <= endIp; ip ++ {
+			guessHost := fmt.Sprintf("%s%d", prefixHost, lastIp + int(ip))
+			guessTarget := fmt.Sprintf("%s:%d", guessHost, port)
+			if guessTarget != address && IsFoundHost(guessHost, port) {
+				neighbors = append(neighbors, guessTarget)
+			}
+		}
+	}
+	return neighbors
 }
