ca279f13b121953ed0e7563bd838b5cd9076704b
tomoyaueno29 <u.t012807@gmail.com>
2020-06-22 13:58:21 +0900

    Update blockchain_server.go

--- blockchain_server/blockchain_server.go
+++ blockchain_server/blockchain_server.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"goblockchain/block"
+	"goblockchain/wallet"
 	"io"
+	"log"
 	"net/http"
 	"strconv"
 )
+
+var cache map[string]*block.Blockchain = make(map[string]*block.Blockchain)
 
 type BlockchainServer struct {
 	port uint16
@@ -18,14 +23,35 @@ func (bcs *BlockchainServer) Port() uint16 {
 	return bcs.port
 }
 
-func HelloWorld(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Hello World")
+func (bcs *BlockchainServer) GetBlockchain() *block.Blockchain {
+	bc, ok := cache["blockchain"]
+	if !ok {
+		minerWallet := wallet.NewWallet()
+		bc = block.NewBlockchain(minerWallet.BlockchainAddress(), bcs.Port())
+		cache["blockchain"] = bc
+		log.Printf("private_key %v", minerWallet.PrivateKeyStr())
+		log.Printf("public_key %v", minerWallet.PublicKeyStr())
+		log.Printf("blockchain_address %v", minerWallet.BlockchainAddress())
+	}
+	return bc
+}
+
+func (bcs *BlockchainServer) GetChain(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodGet:
+		w.Header().Add("Content-Type", "application/json")
+		bc := bcs.GetBlockchain()
+		m, _ := bc.MarshalJSON()
+		io.WriteString(w, string(m[:]))
+	default:
+		log.Println("ERROR: Invalid HTTP Method")
+	}
 }
 
 func (bcs *BlockchainServer) Run() {
 	server := http.Server{
 		Addr: "0.0.0.0:"+strconv.Itoa(int(bcs.port)),
 	}
-	http.HandleFunc("/", HelloWorld)
+	http.HandleFunc("/", bcs.GetChain)
 	server.ListenAndServe()
 }
