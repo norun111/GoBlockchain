9085971a1fc25f04fb2e2b055c5bab59792aae75
tomoyaueno29 <u.t012807@gmail.com>
2020-06-22 13:22:04 +0900

    Update blockchain_server.go

--- blockchain_server/blockchain_server.go
+++ blockchain_server/blockchain_server.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"io"
+	"net/http"
+	"strconv"
+)
+
+type BlockchainServer struct {
+	port uint16
+}
+
+func NewBlockchainServer(port uint16) *BlockchainServer {
+	return &BlockchainServer{port}
+}
+
+func (bcs *BlockchainServer) Port() uint16 {
+	return bcs.port
+}
+
+func HelloWorld(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Hello World")
+}
+
+func (bcs *BlockchainServer) Run() {
+	server := http.Server{
+		Addr: "0.0.0.0:"+strconv.Itoa(int(bcs.port)),
+	}
+	http.HandleFunc("/", HelloWorld)
+	server.ListenAndServe()
+}
