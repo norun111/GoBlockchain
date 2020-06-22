7a6c2a7f2fefb2ff50a10266a2067754e2f80cf6
tomoyaueno29 <u.t012807@gmail.com>
2020-06-22 15:54:38 +0900

    Update wallet_server.go

--- wallet_server/wallet_server.go
+++ wallet_server/wallet_server.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"encoding/json"
+	"goblockchain/utils"
+	"goblockchain/wallet"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
 	"path"
 	"strconv"
+	"fmt"
 )
 
-//"wallet_server/templates"では動かない
 const tempDir = "templates"
 
 type WalletServer struct {
@@ -38,7 +42,65 @@ func (ws *WalletServer) Index(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func (ws *WalletServer) Wallet(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodPost:
+		w.Header().Add("Content-Type", "application/json")
+		myWallet := wallet.NewWallet()
+		m, _ := myWallet.MarshalJSON()
+		io.WriteString(w, string(m[:]))
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		log.Println("ERROR: Invalid HTTP Method")
+	}
+}
+
+func (ws *WalletServer) CreateTransaction(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodPost:
+		log.Println(req.Body)
+		decoder := json.NewDecoder(req.Body)
+		var t wallet.TransactionRequest
+		err := decoder.Decode(&t)
+		if err != nil {
+			log.Printf("ERROR: %v", err)
+			io.WriteString(w, string(utils.JsonStatus("fail")))
+			return
+		}
+		if !t.Validate() {
+			log.Println("ERROR: missing fields")
+			io.WriteString(w, string(utils.JsonStatus("fail")))
+			return
+		}
+
+		publicKey := utils.PublicKeyFromString(*t.SenderPublicKey)
+		privateKey := utils.PrivateKeyFromString(*t.SenderPrivateKey, publicKey)
+		value, err := strconv.ParseFloat(*t.Value, 32)
+		if err != nil {
+			log.Println("ERROR: parse error")
+			io.WriteString(w, string(utils.JsonStatus("fail")))
+			return
+		}
+		value32 := float32(value)
+
+		w.Header().Add("Content-Type","application/json")
+		fmt.Println(publicKey)
+		fmt.Println(privateKey)
+		fmt.Printf("%.1f", value32)
+		//fmt.Println(len(*t.SenderPrivateKey))
+		//fmt.Println(len(*t.SenderPublicKey))
+		//fmt.Println(*t.SenderBlockchainAddress)
+		//fmt.Println(*t.RecipientBlockchainAddress)
+		//fmt.Println(*t.Value)
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		log.Println("ERROR: Invalid HTTP Method")
+	}
+}
+
 func (ws *WalletServer) Run() {
 	http.HandleFunc("/", ws.Index)
+	http.HandleFunc("/wallet", ws.Wallet)
+	http.HandleFunc("/transaction", ws.CreateTransaction)
 	log.Fatal(http.ListenAndServe("0.0.0.0:"+strconv.Itoa(int(ws.Port())), nil))
 }
