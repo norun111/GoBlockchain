9dcc188b81f4e5e8b57f0f4533c947c8d8c0c67e
tomoyaueno29 <u.t012807@gmail.com>
2020-06-22 19:42:24 +0900

    Update neighbor.go

--- utils/neighbor.go
+++ utils/neighbor.go
@@ -23,25 +23,20 @@ func IsFoundHost(host string, port uint16) bool {
 var PATTERN = regexp.MustCompile(`((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?\.){3})(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)`)
 
 func FindNeighbors(myHost string, myPort uint16, startIp uint8, endIp uint8, startPort uint16, endPort uint16) []string {
-	//自分自身のアドレス
 	address := fmt.Sprintf("%s:%d", myHost, myPort)
 
 	m := PATTERN.FindStringSubmatch(myHost)
 	if m == nil {
 		return nil
 	}
-	//ex.) 192.168.0.10:5003
-	prefixHost := m[1] //192.168.0
-	lastIp, _ := strconv.Atoi(m[len(m)-1]) //10
+	prefixHost := m[1]
+	lastIp, _ := strconv.Atoi(m[len(m)-1])
 	neighbors := make([]string, 0)
 
-	//startPort = 5000, endPort = 5002,
 	for port := startPort; port <= endPort; port += 1 {
-		//startIp = 0, endIp = 11, 12, 13
 		for ip := startIp; ip <= endIp; ip += 1 {
 			guessHost := fmt.Sprintf("%s%d", prefixHost, lastIp+int(ip))
 			guessTarget := fmt.Sprintf("%s:%d", guessHost, port)
-			//アドレスが自分自身のものでない時かつもし発見したらneighborsに追加
 			if guessTarget != address && IsFoundHost(guessHost, port) {
 				neighbors = append(neighbors, guessTarget)
 			}
@@ -55,11 +50,9 @@ func GetHost() string {
 	if err != nil {
 		return "127.0.0.1"
 	}
-	//net.LookupHost(name string) (addrs []string, err error)
 	address, err := net.LookupHost(hostname)
 	if err != nil {
 		return "127.0.0.1"
 	}
 	return address[0]
 }
-
