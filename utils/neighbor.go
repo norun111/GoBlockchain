7700fcd89f7214f7b1b648a329159e3014d8bbbd
tomoyaueno29 <u.t012807@gmail.com>
2020-06-22 19:11:47 +0900

    Update neighbor.go

--- utils/neighbor.go
+++ utils/neighbor.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"fmt"
 	"net"
+	"os"
 	"regexp"
 	"strconv"
 	"time"
@@ -22,20 +23,26 @@ func IsFoundHost(host string, port uint16) bool {
 var PATTERN = regexp.MustCompile(`((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?\.){3})(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)`)
 
 func FindNeighbors(myHost string, myPort uint16, startIp uint8, endIp uint8, startPort uint16, endPort uint16) []string {
+	//自分自身のアドレス
 	address := fmt.Sprintf("%s:%d", myHost, myPort)
 
 	m := PATTERN.FindStringSubmatch(myHost)
 	if m == nil {
 		return nil
 	}
-	prefixHost := m[1]
-	lastIp, _ := strconv.Atoi(m[len(m)-1])
+	//ex.) 192.168.0.10:5003
+	prefixHost := m[1] //192.168.0
+	lastIp, _ := strconv.Atoi(m[len(m)-1]) //10
 	neighbors := make([]string, 0)
 
+	//startPort = 5000, endPort = 5002,
 	for port := startPort; port <= endPort; port ++ {
+
+		//startIp = 0, endIp = 11, 12, 13
 		for ip := startIp; ip <= endIp; ip ++ {
 			guessHost := fmt.Sprintf("%s%d", prefixHost, lastIp + int(ip))
 			guessTarget := fmt.Sprintf("%s:%d", guessHost, port)
+			//アドレスが自分自身のものでない時かつもし発見したらneighborsに追加
 			if guessTarget != address && IsFoundHost(guessHost, port) {
 				neighbors = append(neighbors, guessTarget)
 			}
@@ -43,3 +50,18 @@ func FindNeighbors(myHost string, myPort uint16, startIp uint8, endIp uint8, sta
 	}
 	return neighbors
 }
+
+func GetHost() string {
+	hostname, err := os.Hostname()
+	if err != nil {
+		return "127.0.0.1"
+	}
+	//net.LookupHost(name string) (addrs []string, err error)
+	address, err := net.LookupHost(hostname)
+	if err != nil {
+		return "127.0.0.1"
+	}
+
+	return address[0]
+}
+
