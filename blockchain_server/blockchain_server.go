e2d56f6253a5a749840c5032580f55f7ef54ca13
tomoyaueno29 <u.t012807@gmail.com>
2020-06-22 16:33:34 +0900

    Update blockchain_server.go

--- blockchain_server/blockchain_server.go
+++ blockchain_server/blockchain_server.go
@@ -70,6 +70,20 @@ func (bcs *BlockchainServer) Transaction(w http.ResponseWriter, req *http.Reques
 		}
 		publicKey := utils.PublicKeyFromString(*t.SenderPublicKey)
 		signature := utils.SignatureFromString(*t.Signature)
+		bc := bcs.GetBlockchain()
+		isCreated := bc.CreateTransaction(*t.SenderBlockchainAddress,
+			*t.RecipientBlockChainAddress, *t.Value, publicKey, signature)
+
+		w.Header().Add("Content-Type", "application/json")
+		var m []byte
+		if !isCreated {
+			w.WriteHeader(http.StatusBadRequest)
+			m = utils.JsonStatus("fail")
+		} else {
+			w.WriteHeader(http.StatusCreated)
+			m = utils.JsonStatus("success")
+		}
+		io.WriteString(w, string(m))
 
 	default:
 		log.Println(":ERROR: Invalid HTTP Method")
