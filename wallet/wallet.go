2ec6a4d31adee8817b0a4bce4f8f586440b26a52
tomoyaueno29 <u.t012807@gmail.com>
2020-06-22 12:50:25 +0900

    Update wallet.go

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -114,12 +114,3 @@ func (t *Transaction) MarshalJSON() ([]byte, error) {
 		Value: t.value,
 	})
 }
-
-type Signature struct {
-	R *big.Int //一般的な公開鍵のx座標
-	S *big.Int
-}
-
-func (s *Signature) String() string {
-	return fmt.Sprintf("%x%x", s.R, s.S)
-}
