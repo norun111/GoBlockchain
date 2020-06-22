1c2c69ca5679066f1c8339b770e9b8f33e199305
tomoyaueno29 <u.t012807@gmail.com>
2020-06-22 12:50:23 +0900

    Update ecdsa.go

--- utils/ecdsa.go
+++ utils/ecdsa.go
@@ -1,1 +1,15 @@
 package utils
+
+import (
+	"math/big"
+	"fmt"
+)
+
+type Signature struct {
+	R *big.Int //一般的な公開鍵のx座標
+	S *big.Int
+}
+
+func (s *Signature) String() string {
+	return fmt.Sprintf("%x%x", s.R, s.S)
+}
