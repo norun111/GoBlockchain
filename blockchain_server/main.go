05fbd4fac9476168bce02e32b4ba429694bf5f7f
tomoyaueno29 <u.t012807@gmail.com>
2020-06-22 19:42:21 +0900

    Update main.go

--- blockchain_server/main.go
+++ blockchain_server/main.go
@@ -2,18 +2,16 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 )
 
 func init() {
-	log.SetPrefix("Blockchain:")
+	log.SetPrefix("Blockchain: ")
 }
 
 func main() {
-	port := flag.Uint("port", 5000, "TCP Number for Blockchain Server")
+	port := flag.Uint("port", 5000, "TCP Port Number for Blockchain Server")
 	flag.Parse()
-	fmt.Println(*port)
 	app := NewBlockchainServer(uint16(*port))
 	app.Run()
 }
